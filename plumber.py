ff75fb577c79a5012a8d0e952d8558481e1ca5d4
PurpleSPC <108835910+PurpleSPC@users.noreply.github.com>
2025-06-24 22:50:55 -0400

    output cleaned and sent to csv file

--- plumber.py
+++ plumber.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import csv
 
 def main():
 
@@ -6,9 +7,47 @@ def main():
         page = pdf.pages[0]
         tables = page.extract_tables()
 
+    for table in tables:
+        if "Rep ID#" in table[0]:
+            rep_table = table
+        elif any(item.startswith("Account Name") for item in table[0] if item):
+            account_table = table
+        elif any(item.startswith("Date") for item in table[0] if item):
+            case_table = table
+        elif "Catalog #" in table[0]:
+            sales_table = table
+        elif "Ship To Acct #" in table[0]:
+            restock_table = table
+
+    rep_name = cleaner(rep_table[0][0])
+    account_name = cleaner(account_table[0][0])
+    case_date = cleaner(case_table[0][0])
+    surgeon = cleaner(case_table[0][1])
+    total_amt = cleaner(restock_table[2][3])
+    restock_loc = cleaner(restock_table[0][0])
+
+    data = [
+        ["Case Date", "Surgeon", "Account Name", "Rep Name","Billed Amount","Restock Location"],
+        [case_date,surgeon,account_name,rep_name,total_amt,restock_loc]
+    ]
+
+    with open("output.csv","w",newline=None) as csvfile:
+        writer = csv.writer(csvfile)
+        writer.writerows(data)
 
 
 
-
+def cleaner(field):
+    extras = [
+        "Account Name",
+        "Date",
+        "Surgeon (Last, First, MI)",
+        "First and Last Name (or Loaner Set ID#)",
+        "Name"
+    ]
+    for extra in extras:
+        if field.startswith(extra):
+            field = field[len(extra):].strip()
+    return field
 
 main()
